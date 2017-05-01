a00eda20c3a52f3bb02f41ae9b7e8cc9dee0bf0c
paul42 <paul.42@gmail.com>
2017-05-01 13:04:13 -0700

    adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
