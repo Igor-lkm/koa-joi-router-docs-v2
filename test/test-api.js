b55f162f9bd4150ad5d81e508b9d5b32e7ef24ef
Edward Chu <crazyzyt@gmail.com>
2017-09-18 11:04:42 +0800

    add: test

--- test/test-api.js
+++ test/test-api.js
@@ -1,14 +1,12 @@
-'use strict';
-const assert = require('power-assert');
+const assert = require('power-assert')
 const Router = require('koa-joi-router')
 const Joi = Router.Joi
 
-const { SwaggerAPI } = require('../');
+const { SwaggerAPI } = require('../')
 
 describe('API', function () {
   it('should success with valid data', function () {
-    const generator = new SwaggerAPI();
-
+    const generator = new SwaggerAPI()
     const router = Router()
 
     router.get('/signup', {
@@ -30,19 +28,75 @@ describe('API', function () {
           }
         }
       },
-      handler: async() => {}
+      handler: async () => {}
     })
 
-    generator.addJoiRouter(router);
-
+    generator.addJoiRouter(router)
     const spec = generator.generateSpec({
       info: {
         title: 'Example API',
         version: '1.1'
       },
-      basePath: '/api'
+      basePath: '/'
     })
-    
     assert(['info', 'basePath', 'swagger', 'paths', 'tags'].every(v => v in spec))
-  });
-});
+  })
+
+  it('should success with empty default response', function () {
+    const generator = new SwaggerAPI()
+    const router = Router()
+
+    router.get('/empty-default-response', {
+      validate: {
+        output: {
+          201: {
+            body: {
+              ok: Joi.bool()
+            }
+          }
+        }
+      },
+      handler: async () => {}
+    })
+
+    generator.addJoiRouter(router)
+    const spec = generator.generateSpec({
+      info: {
+        title: 'Example API',
+        version: '1.1'
+      },
+      basePath: '/'
+    }, { defaultResponses: null })
+    assert(!('200' in spec.paths['/empty-default-response'].get.responses))
+  })
+
+  it('should success with output placed outside of validate', function () {
+    const generator = new SwaggerAPI()
+    const router = Router()
+
+    router.get('/output-outside-validate', {
+      validate: {
+        output: {
+          201: {
+            body: {
+              ok: Joi.bool()
+            }
+          }
+        }
+      },
+      handler: async () => {}
+    })
+
+    generator.addJoiRouter(router)
+    const spec = generator.generateSpec({
+      info: {
+        title: 'Example API',
+        version: '1.1'
+      },
+      basePath: '/'
+    })
+    assert(
+      ['200', '201'].every(v => v in spec.paths['/output-outside-validate'].get.responses)
+    )
+  })
+})
